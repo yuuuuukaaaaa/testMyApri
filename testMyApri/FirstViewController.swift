f7a6e5ee4a75f09f65f37c8e2c529f96123cd2f4
MacUser <fea2gg@gmail.com>
2017-06-27 21:35:06 +0800

    初更新

---
+++ testMyApri/FirstViewController.swift
@@ -0,0 +1,54 @@
+//
+//  FirstViewController.swift
+//  testMyApri
+//
+//  Created by MacUser on 2017/06/18.
+//  Copyright © 2017年 Yuka Saito. All rights reserved.
+//
+
+import UIKit
+
+//1プロトコルの設定
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    @IBOutlet weak var myPickerView: UITableView!
+    
+    override func viewDidLoad() {
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
+    }
+    
+////    self.インスタンスメソッド
+////    
+////    delegate.インスタンスメソッド
+////    
+////    インスタンス。いんすたんすめそっど
+////    
+////    いんすたんす（UITableViewCellのインスタンス）
+////    いんすたんすめそっど（UITableViewCellくらすにていぎしてあるめそっど）
+//    
+//    myPickerView.delegate.tableView(_ tableView: UITableView, )
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+//        tableView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
+//        cell.accessoryType = UITableViewCellAccessoryType.checkmark
+//        
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+     return 3
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
