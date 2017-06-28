69469e2dd87b9aa86c794d2abb248a2d6b5c4eab
MacUser <fea2gg@gmail.com>
2017-06-28 11:50:46 +0800

    画面遷移ロジック追加

--- testMyApri/FirstViewController.swift
+++ testMyApri/FirstViewController.swift
@@ -1,4 +1,4 @@
-//
+
 //  FirstViewController.swift
 //  testMyApri
 //
@@ -10,40 +10,44 @@ import UIKit
 
 //1プロトコルの設定
 class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
-    
+
     @IBOutlet weak var myPickerView: UITableView!
+
+    @IBOutlet weak var addButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         myPickerView.delegate = self
         myPickerView.dataSource = self
+        
+        
     }
-    
+
 ////    self.インスタンスメソッド
-////    
+////
 ////    delegate.インスタンスメソッド
-////    
+////
 ////    インスタンス。いんすたんすめそっど
-////    
+////
 ////    いんすたんす（UITableViewCellのインスタンス）
 ////    いんすたんすめそっど（UITableViewCellくらすにていぎしてあるめそっど）
-//    
+//
 //    myPickerView.delegate.tableView(_ tableView: UITableView, )
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        
+
 //        tableView.register(<#T##cellClass: AnyClass?##AnyClass?#>, forCellReuseIdentifier: <#T##String#>)
-//        cell.accessoryType = UITableViewCellAccessoryType.checkmark
-//        
-        
-        
+//       cell.accessoryType = UITableViewCellAccessoryType.checkmark
+//
+
+
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
      return 3
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -51,4 +55,10 @@ class FirstViewController: UIViewController, UITableViewDataSource, UITableViewD
 
 
 }
+
+
+
+
+
+
 
--- testMyApri/ThirdViewController.swift
+++ testMyApri/ThirdViewController.swift
@@ -8,13 +8,63 @@
 
 import UIKit
 
-class ThirdViewController: UIViewController {
 
+class ThirdViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    
+    //    データを配列で用意する（メンバ変数　どこでも使える変数！）
+    var foodArray = ["ダージリン","アッサム","アールグレイ","ロイヤル・ブレックファースト"]
+    //選択された行番号
+    var selectedIndex = -1
+    //TODO:テーブルビューに紅茶のリストを表示しましょう
+    //文字色は紅茶がイメージできる色（黒以外）
+    //右側にアクセサリ（＞みたいなマークをつけましょう）
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //2　行数を設定
+    //-> Int：戻り値のデータ型はInt型ですという意味　ついているものは戻り値がある
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foodArray.count
+        
+    }
+    
+    //3 リストに表示する文字列行数を決定表示
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            cell.textLabel?.text = foodArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //セルがタップ（選択）された時
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print("選択されたデータ:\(foodArray[indexPath.row])")
+        //選択されたメンバ変数行番号をメンバ変数に格納
+        selectedIndex = indexPath.row
+        
+        //セグエを指定して画面移動
+        performSegue(withIdentifier: "showDetail", sender: nil)
+    }
+    
+    //セグエを通って次の画面へ移動するとき
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //次の画面をインスタンス化(as:ダウンキャスト型変換)
+        
+       // var dvc = segue.destination as ditailViewController
+        
+        //次の画面のプロパティに選択された行番号を指定
+      //  dvc.sIndex = selectedIndex
+        
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
